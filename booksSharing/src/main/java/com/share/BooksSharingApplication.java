798381fd40a54f10a86e720ffa963955761707bc
yingmengxuepingbao <1163003193@qq.com>
2019-08-21 15:13:16 +0800

    搭建spring boot框架

---
+++ booksSharing/src/main/java/com/share/BooksSharingApplication.java
@@ -0,0 +1,13 @@
+package com.share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksSharingApplication.class, args);
+	}
+
+}
